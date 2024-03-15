220cd0e63351299978a14f5d08e3871ed07c76c2
David Lattimore <dvdlttmr@gmail.com>
2024-03-15 14:37:56 +1100

    Be more selective with which debug sections --strip-debug strips
    We still might need sections like .debug_gdb_scripts if they're
    referenced.

--- wild_lib/src/output_section_id.rs
+++ wild_lib/src/output_section_id.rs
@@ -527,7 +527,7 @@ impl<'data> UnloadedSection<'data> {
             // We don't currently allow references to these sections, discard them so that we avoid
             // allocating output section IDs.
             None
-        } else if args.strip_debug && section_name.starts_with(b".debug_") {
+        } else if args.strip_debug && section_name == b".debug_str" {
             None
         } else {
             let ty = match section.kind() {
