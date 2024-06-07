080ffed88b2887d045706a95d0086dd25f48aef6
David Lattimore <dvdlttmr@gmail.com>
2024-06-07 13:48:44 +1000

    Test that Rust panics work

--- wild/tests/sources/rust-integration.rs
+++ wild/tests/sources/rust-integration.rs
@@ -6,8 +6,15 @@
 //#CompArgs:--target x86_64-unknown-linux-musl -C relocation-model=static -C target-feature=+crt-static -C debuginfo=2
 
 //#Config:cranelift:default
-//#CompArgs:-Zcodegen-backend=cranelift --target x86_64-unknown-linux-musl -C relocation-model=static -C target-feature=+crt-static -C debuginfo=2
+//#CompArgs:-Zcodegen-backend=cranelift --target x86_64-unknown-linux-musl -C relocation-model=static -C target-feature=+crt-static -C debuginfo=2 --cfg cranelift
+
+fn foo() {
+    panic!("Make sure unwinding works");
+}
 
 fn main() {
+    if !cfg!(cranelift) && std::panic::catch_unwind(foo).is_ok() {
+        std::process::exit(101);
+    }
     std::process::exit(42);
 }
