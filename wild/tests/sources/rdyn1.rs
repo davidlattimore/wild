514f1d5e627d7cc9ec3ec1377ed56de072e8cbba
David Lattimore <dvdlttmr@gmail.com>
2024-06-27 12:02:36 +1000

    integration-tests: Expand use of TLS in rust integration test

--- wild/tests/sources/rdyn1.rs
+++ wild/tests/sources/rdyn1.rs
@@ -15,6 +15,10 @@ thread_local! {
     pub static TLS1: Cell<i32> = const { Cell::new(1) };
 }
 
+thread_local! {
+    pub static TLS2: Cell<i32> = const { Cell::new(2) };
+}
+
 #[no_mangle]
 pub extern fn get_tls1() -> i32 {
     TLS1.get()
@@ -23,4 +27,14 @@ pub extern fn get_tls1() -> i32 {
 #[no_mangle]
 pub extern fn set_tls1(value: i32) {
     TLS1.set(value);
+}
+
+#[no_mangle]
+pub extern fn get_tls2() -> i32 {
+    TLS2.get()
+}
+
+#[no_mangle]
+pub extern fn set_tls2(value: i32) {
+    TLS2.set(value);
 }
--- wild/tests/sources/rust-integration-dynamic.rs
+++ wild/tests/sources/rust-integration-dynamic.rs
@@ -5,11 +5,13 @@
 //#CompArgs:-C debuginfo=2
 //#Shared:rdyn1.rs
 
-extern {
+extern "C" {
     fn foo() -> i32;
     fn bar() -> i32;
     fn get_tls1() -> i32;
     fn set_tls1(value: i32);
+    fn get_tls2() -> i32;
+    fn set_tls2(value: i32);
 }
 
 fn main() {
@@ -21,9 +23,25 @@ fn main() {
         std::process::exit(101);
     }
 
-    unsafe { set_tls1(88); }
-    if unsafe { get_tls1() } != 88 {
+    if unsafe { get_tls1() } != 1 {
         std::process::exit(102);
+    }
+    if unsafe { get_tls2() } != 2 {
+        std::process::exit(103);
+    }
+
+    unsafe {
+        set_tls1(88);
+    }
+    unsafe {
+        set_tls2(55);
+    }
+
+    if unsafe { get_tls1() } != 88 {
+        std::process::exit(104);
+    }
+    if unsafe { get_tls2() } != 55 {
+        std::process::exit(105);
     }
 
     std::process::exit(42);
