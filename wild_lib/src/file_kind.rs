49a94921ce1d687a875d8c0eedfa9799691a5150
Martin Liska <martin.liska@hey.com>
2024-08-26 20:33:58 +0200

    Use more object::elf constants

--- wild_lib/src/file_kind.rs
+++ wild_lib/src/file_kind.rs
@@ -19,21 +19,21 @@ impl FileKind {
     pub(crate) fn identify_bytes(bytes: &[u8]) -> Result<FileKind> {
         if bytes.starts_with(b"!<arch>") {
             Ok(FileKind::Archive)
-        } else if bytes.starts_with(&[0x7f, b'E', b'L', b'F']) {
+        } else if bytes.starts_with(&object::elf::ELFMAG) {
             const HEADER_LEN: usize = std::mem::size_of::<elf::FileHeader>();
             if bytes.len() < HEADER_LEN {
                 bail!("Invalid ELF file");
             }
             let header: &elf::FileHeader = object::from_bytes(&bytes[..HEADER_LEN]).unwrap().0;
-            if header.e_ident.class != 2 {
+            if header.e_ident.class != object::elf::ELFCLASS64 {
                 bail!("Only 64 bit ELF is currently supported");
             }
-            if header.e_ident.data != 1 {
+            if header.e_ident.data != object::elf::ELFDATA2LSB {
                 bail!("Only little endian is currently supported");
             }
             match header.e_type.get(LittleEndian) {
-                1 => Ok(FileKind::ElfObject),
-                3 => Ok(FileKind::ElfDynamic),
+                object::elf::ET_REL => Ok(FileKind::ElfObject),
+                object::elf::ET_DYN => Ok(FileKind::ElfDynamic),
                 t => bail!("Unsupported ELF kind {t}"),
             }
         } else if bytes.is_ascii() {
