fe307369b9b815ea5226fe1c2eac7b2e0f9beedd
Martin Liska <martin.liska@hey.com>
2024-08-26 06:43:52 +0200

    Address new clippy warning:
    https://rust-lang.github.io/rust-clippy/master/index.html#/too_long_first_doc_paragraph

--- wild_lib/src/error.rs
+++ wild_lib/src/error.rs
@@ -2,7 +2,9 @@ pub(crate) use anyhow::Error;
 
 pub type Result<T = (), E = Error> = core::result::Result<T, E>;
 
-/// Like debug_assert, but bails instead of panicking. Returning an error often allows us to give
+/// Like debug_assert, but bails instead of panicking.
+///
+/// Returning an error often allows us to give
 /// more context as to what we were trying to do, e.g. which file / symbol we were processing,
 /// whereas a panic just gives us a function backtrace, which is less useful.
 #[macro_export]
