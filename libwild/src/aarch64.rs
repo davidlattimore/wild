22415bbf67521b18beb46e9d5b01973a40cda708
Martin Liska <martin.liska@hey.com>
2025-01-22 06:25:25 +0100

    Extend support for TLSDESC to AArch64

--- libwild/src/aarch64.rs
+++ libwild/src/aarch64.rs
@@ -746,6 +746,64 @@ impl crate::arch::Arch for AArch64 {
                 None,
             ),
 
+            // 5.7.11.5 Thread-local storage descriptors
+            object::elf::R_AARCH64_TLSDESC_LD_PREL19 => (
+                RelocationKind::TlsDesc,
+                RelocationSize::BitMasking {
+                    range: BitRange { start: 2, end: 21 },
+                    insn: RelocationInstruction::Ldr,
+                },
+                None,
+            ),
+            object::elf::R_AARCH64_TLSDESC_ADR_PREL21 => (
+                RelocationKind::TlsDesc,
+                RelocationSize::BitMasking {
+                    range: BitRange { start: 0, end: 21 },
+                    insn: RelocationInstruction::Adr,
+                },
+                None,
+            ),
+            object::elf::R_AARCH64_TLSDESC_ADR_PAGE21 => (
+                RelocationKind::TlsDesc,
+                RelocationSize::BitMasking {
+                    range: BitRange { start: 12, end: 33 },
+                    insn: RelocationInstruction::Adr,
+                },
+                Some(PageMask::GotEntryAndPosition),
+            ),
+            object::elf::R_AARCH64_TLSDESC_LD64_LO12 => (
+                RelocationKind::TlsDescGot,
+                RelocationSize::BitMasking {
+                    range: BitRange { start: 3, end: 12 },
+                    insn: RelocationInstruction::Ldr,
+                },
+                None,
+            ),
+            object::elf::R_AARCH64_TLSDESC_ADD_LO12 => (
+                RelocationKind::TlsDescGot,
+                RelocationSize::BitMasking {
+                    range: BitRange { start: 0, end: 12 },
+                    insn: RelocationInstruction::Add,
+                },
+                None,
+            ),
+            object::elf::R_AARCH64_TLSDESC_OFF_G1 => (
+                RelocationKind::TlsDescGotBase,
+                RelocationSize::BitMasking {
+                    range: BitRange { start: 16, end: 32 },
+                    insn: RelocationInstruction::Movnz,
+                },
+                None,
+            ),
+            object::elf::R_AARCH64_TLSDESC_OFF_G0_NC => (
+                RelocationKind::TlsDescGotBase,
+                RelocationSize::BitMasking {
+                    range: BitRange { start: 0, end: 16 },
+                    insn: RelocationInstruction::Movkz,
+                },
+                None,
+            ),
+
             _ => bail!(
                 "Unsupported relocation type {}",
                 Self::rel_type_to_string(r_type)
--- wild/tests/sources/tlsdesc.c
+++ wild/tests/sources/tlsdesc.c
@@ -9,24 +9,49 @@
 //#CompArgs:-mtls-dialect=gnu2
 //#LinkArgs:--cc=gcc -Wl,-z,now
 //#Object:tlsdesc-obj.c
+//#Arch: x86_64
+
+//#Config:gcc-tls-desc-aarch64:gcc-tls-desc
+//#CompArgs:-mtls-dialect=desc
+//#Arch: aarch64
 
 //#Config:gcc-tls-desc-pie:gcc-tls-desc
 //#CompArgs:-mtls-dialect=gnu2 -fPIE
+//#Arch: x86_64
+
+//#Config:gcc-tls-desc-pie-aarch64:gcc-tls-desc-pie
+//#CompArgs:-mtls-dialect=desc
+//#Arch: aarch64
 
 //#Config:gcc-tls-desc-shared:gcc-tls-desc
 //#CompArgs:-mtls-dialect=gnu2 -fPIC
 //#Shared:tlsdesc-obj.c
 //#DiffIgnore:asm.get_value
+//#Arch: x86_64
+
+//#Config:gcc-tls-desc-shared-aarch64:gcc-tls-desc-shared
+//#CompArgs:-mtls-dialect=desc
+//#Arch: aarch64
 
 //#Config:clang-tls-desc:gcc-tls-desc
 //#CompArgs:-mtls-dialect=gnu2
 //#Compiler:clang
 //#RequiresClangWithTlsDesc:true
+//#Arch: x86_64
+
+//#Config:clang-tls-desc-aarch64:clang-tls-desc
+//#CompArgs:-mtls-dialect=desc
+//#Arch: aarch64
 
 //#Config:clang-tls-desc-shared:clang-tls-desc
 //#CompArgs:-mtls-dialect=gnu2 -fPIC
 //#Shared:tlsdesc-obj.c
 //#DiffIgnore:asm.get_value
+//#Arch: x86_64
+
+//#Config:clang-tls-desc-shared-aarch64:clang-tls-desc-shared
+//#CompArgs:-mtls-dialect=desc
+//#Arch: aarch64
 
 int get_value();
 
