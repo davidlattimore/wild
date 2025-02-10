6a36ad4a74fd23ccb8dae9af47a81df26f40d561
Martin Liska <martin.liska@hey.com>
2025-02-10 20:44:11 +0100

    Fix relaxation condition

--- libwild/src/aarch64.rs
+++ libwild/src/aarch64.rs
@@ -132,7 +132,7 @@ impl crate::arch::Relaxation for Relaxation {
         // away fetching it.
 
         match relocation_kind {
-            object::elf::R_AARCH64_CALL26 | object::elf::R_AARCH64_JUMP26 => {
+            object::elf::R_AARCH64_CALL26 | object::elf::R_AARCH64_JUMP26 if can_bypass_got => {
                 relocation.kind = RelocationKind::Relative;
                 return Some(Relaxation {
                     kind: RelaxationKind::NoOp,
