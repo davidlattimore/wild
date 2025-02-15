dda1c22c47d0bb8e67123328fd4c757f57fad54e
riverbl <94326797+riverbl@users.noreply.github.com>
2025-02-15 17:44:03 +0000

    Add `--whole-archive`

---
+++ wild/tests/sources/whole_archive.c
@@ -0,0 +1,19 @@
+//#Object:exit.c
+
+//#LinkArgs:--whole-archive
+//#Archive:whole_archive0.c
+
+#include "exit.h"
+
+extern int __start_foo[];
+extern int __stop_foo[];
+
+void _start(void) {
+    int value = 0;
+
+    for (int *foo = __start_foo; foo < __stop_foo; ++foo) {
+        value += *foo;
+    }
+
+    exit_syscall(value);
+}
