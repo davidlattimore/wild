52c979a7cf83f0a13a948658cba130a260f026d9
Martin Liska <martin.liska@hey.com>
2025-02-26 18:20:23 +0100

    Fix config names

--- wild/tests/sources/tls-variant.c
+++ wild/tests/sources/tls-variant.c
@@ -17,14 +17,14 @@
 //#Shared:tls-variant-1.c:-mtls-dialect=gnu2,tls-variant-2.c:-ftls-model=global-dynamic,tls-variant-3.c:-ftls-model=initial-exec
 //#Arch: x86_64
 
-//#Config:gcc:default
+//#Config:gcc-aarch64:default
 //#CompArgs:-fpic
 //#Object:tls-variant-1.c
 //#Object:tls-variant-2.c:-ftls-model=global-dynamic -mtls-dialect=trad
 //#Object:tls-variant-3.c:-ftls-model=initial-exec -mtls-dialect=trad
 //#Arch: aarch64
 
-//#Config:gcc-shared:default
+//#Config:gcc-shared-aarch64:default
 //#CompArgs:-fpic
 //#Shared:tls-variant-1.c,tls-variant-2.c:-ftls-model=global-dynamic -mtls-dialect=trad,tls-variant-3.c:-ftls-model=initial-exec -mtls-dialect=trad
 //#Arch: aarch64
