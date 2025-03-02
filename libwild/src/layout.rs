53ceee45bc6640bab433940672592fd79397d947
Mateusz Mikuła <oss@mateuszmikula.dev>
2025-03-02 19:09:54 +0100

    Try yet another approach to Version Script

--- libwild/src/layout.rs
+++ libwild/src/layout.rs
@@ -3242,8 +3242,8 @@ impl<'data> EpilogueLayoutState<'data> {
                 dependencies_count,
             });
 
-            for (_, name, _) in symbol_db.version_script.version_iter() {
-                common.allocate(part_id::DYNSTR, name.len() as u64 + 1);
+            for version in symbol_db.version_script.version_iter() {
+                common.allocate(part_id::DYNSTR, version.name.len() as u64 + 1);
             }
         }
 
@@ -3345,16 +3345,16 @@ impl<'data> EpilogueLayoutState<'data> {
                 is_base: true,
             });
 
-            for (i, (_, name, parent)) in resources
+            for (i, version) in resources
                 .symbol_db
                 .version_script
                 .version_iter()
                 .enumerate()
             {
                 verdefs.push(VersionDef {
-                    name: name.as_bytes().to_vec(),
+                    name: version.name.as_bytes().to_vec(),
                     index: (i + 2) as u16,
-                    parent_name: parent.map(|p| p.as_bytes().to_vec()),
+                    parent_name: version.parent.map(|p| p.as_bytes().to_vec()),
                     is_base: false,
                 });
             }
