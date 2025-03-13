b44198cba583444fb28ad3b52ab6d56d03ddb4c8
David Lattimore <dvdlttmr@gmail.com>
2025-03-13 16:01:25 +1100

    Reduce layers of borrowing
    From the perspective of the command-line interface, this shouldn't change any
    observable behaviour.
    
    From the perspective of the libwild API, it's substantially altered.
    
    For input data and groups, we now keep the prelude and epilogue more separate
    than we did before. This helps because prelude in particular has different
    lifetime constraints than regular object files.
    
    We make use of a typed arena to hold our input data. This allows us to read it,
    then borrow from it.
    
    Another strategy that's employed is passing ownership to the next stage rather
    than having the next stage borrow the previous stage.
    
    The end effect is that we now have a `Linker` struct and running the linker
    borrows from `Linker` and from `Args` and returns a `LinkerOuput`. This means
    that cleanup can be done outside of the main link function, which means that we
    no longer need to pass a `done_callback` into linking.

--- libwild/src/subprocess.rs
+++ libwild/src/subprocess.rs
@@ -1,4 +1,4 @@
-use crate::Linker;
+use crate::Args;
 use crate::error::Result;
 use anyhow::Context as _;
 use anyhow::anyhow;
@@ -21,8 +21,8 @@ use std::io::Error;
 /// # Safety
 /// Must not be called once threads have been spawned. Calling this function from main is generally
 /// the best way to ensure this.
-pub unsafe fn run_in_subprocess(linker: &Linker) -> ! {
-    let exit_code = match subprocess_result(linker) {
+pub unsafe fn run_in_subprocess(args: &Args) -> ! {
+    let exit_code = match subprocess_result(args) {
         Ok(code) => code,
         Err(error) => {
             eprintln!("Error: {error:?}");
@@ -32,7 +32,7 @@ pub unsafe fn run_in_subprocess(linker: &Linker) -> ! {
     std::process::exit(exit_code);
 }
 
-fn subprocess_result(linker: &Linker) -> Result<i32> {
+fn subprocess_result(args: &Args) -> Result<i32> {
     let mut fds: [c_int; 2] = [0; 2];
     // create the pipe used to communicate between the parent and child processes - exit on failure
     make_pipe(&mut fds).context("make_pipe")?;
@@ -43,15 +43,19 @@ fn subprocess_result(linker: &Linker) -> Result<i32> {
     match unsafe { fork() } {
         0 => {
             // Fork success in child - Run linker in this process.
-            linker.setup_tracing()?;
-            linker.setup_thread_pool()?;
-            let done_closure = move || inform_parent_done(&fds);
-            linker.run_with_callback(Some(Box::new(done_closure)))?;
+            crate::setup_tracing(args)?;
+            crate::setup_thread_pool(args)?;
+            let linker = crate::Linker::new();
+            let _outputs = linker.run(args)?;
+            inform_parent_done(&fds);
             Ok(0)
         }
         -1 => {
             // Fork failure in the parent - Fallback to running linker in this process
-            linker.run()?;
+            crate::setup_tracing(args)?;
+            crate::setup_thread_pool(args)?;
+            let linker = crate::Linker::new();
+            linker.run(args)?;
             Ok(0)
         }
         pid => {
--- libwild/src/subprocess_unsupported.rs
+++ libwild/src/subprocess_unsupported.rs
@@ -1,9 +1,10 @@
-use crate::Linker;
+use crate::Args;
+use crate::error::Result;
 
 /// # Safety
 /// See function of the same name in `subprocess.rs`
-pub unsafe fn run_in_subprocess(linker: &Linker) -> ! {
-    let exit_code = match linker.run() {
+pub unsafe fn run_in_subprocess(args: &crate::Args) -> ! {
+    let exit_code = match run_with_args(args) {
         Ok(()) => 0,
         Err(error) => {
             eprintln!("{error}");
@@ -11,4 +12,12 @@ pub unsafe fn run_in_subprocess(linker: &Linker) -> ! {
         }
     };
     std::process::exit(exit_code);
+}
+
+fn run_with_args(args: &Args) -> Result {
+    crate::setup_tracing(args)?;
+    crate::setup_thread_pool(args)?;
+    let linker = crate::Linker::new();
+    linker.run(args)?;
+    Ok(())
 }
