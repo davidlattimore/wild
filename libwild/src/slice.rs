2357f0055311a50884e69368a47904a2aef3025a
Martin Liška <martin.liska@hey.com>
2025-05-16 08:50:07 +0200

    Use new splitting slice API (#763)

--- libwild/src/slice.rs
+++ libwild/src/slice.rs
@@ -1,41 +1,25 @@
-use std::mem::take;
+use anyhow::Context;
 
-/// Removes `prefix` elements from `data` and returns them. Once `take_mut` on core::slice is
-/// stable, we can use that instead.
+/// Removes `prefix` elements from `data` and returns them.
 #[track_caller]
 pub(crate) fn slice_take_prefix_mut<'t, T>(data: &mut &'t mut [T], prefix: usize) -> &'t mut [T] {
-    let len = data.len();
-    assert!(
-        prefix <= len,
-        "Attempted to slice {prefix} elements when only {len} available"
-    );
-    let owned_data = take(data);
-    let (prefix, rest) = owned_data.split_at_mut(prefix);
-    *data = rest;
-    prefix
+    data.split_off_mut(..prefix)
+        .with_context(|| {
+            format!(
+                "Attempted to slice {prefix} elements when only {len} available",
+                len = data.len()
+            )
+        })
+        .unwrap()
 }
 
 pub(crate) fn try_slice_take_prefix_mut<'t, T>(
     data: &mut &'t mut [T],
     prefix: usize,
 ) -> Option<&'t mut [T]> {
-    let len = data.len();
-    if prefix > len {
-        return None;
-    }
-    let owned_data = take(data);
-    let (prefix, rest) = owned_data.split_at_mut(prefix);
-    *data = rest;
-    Some(prefix)
+    data.split_off_mut(..prefix)
 }
 
 pub(crate) fn take_first_mut<'t, T>(data: &mut &'t mut [T]) -> Option<&'t mut T> {
-    if data.is_empty() {
-        None
-    } else {
-        let owned_data = take(data);
-        let (prefix, rest) = owned_data.split_at_mut(1);
-        *data = rest;
-        Some(&mut prefix[0])
-    }
+    data.split_off_first_mut()
 }
