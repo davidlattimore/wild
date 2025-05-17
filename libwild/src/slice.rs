4552c2eb32aca28420d5110cc1bf74055fe9342d
m-hugo <56653363+m-hugo@users.noreply.github.com>
2025-05-17 21:45:39 +0200

    Fix CI

--- libwild/src/slice.rs
+++ libwild/src/slice.rs
@@ -1,13 +1,12 @@
 /// Removes `prefix` elements from `data` and returns them.
 #[track_caller]
 pub(crate) fn slice_take_prefix_mut<'t, T>(data: &mut &'t mut [T], prefix: usize) -> &'t mut [T] {
-    data.split_off_mut(..prefix)
-        .unwrap_or_else(|| {
-            panic!(
-                "Attempted to slice {prefix} elements when only {len} available",
-                len = data.len()
-            )
-        })
+    data.split_off_mut(..prefix).unwrap_or_else(|| {
+        panic!(
+            "Attempted to slice {prefix} elements when only {len} available",
+            len = data.len()
+        )
+    })
 }
 
 pub(crate) fn try_slice_take_prefix_mut<'t, T>(
