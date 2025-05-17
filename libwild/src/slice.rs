e51bfbe424bc8678652aba33394fac856201f6ba
m-hugo <56653363+m-hugo@users.noreply.github.com>
2025-05-17 21:57:32 +0200

    remove useless use of anyhow from slice.rs (#764)

--- libwild/src/slice.rs
+++ libwild/src/slice.rs
@@ -1,16 +1,12 @@
-use anyhow::Context;
-
 /// Removes `prefix` elements from `data` and returns them.
 #[track_caller]
 pub(crate) fn slice_take_prefix_mut<'t, T>(data: &mut &'t mut [T], prefix: usize) -> &'t mut [T] {
-    data.split_off_mut(..prefix)
-        .with_context(|| {
-            format!(
-                "Attempted to slice {prefix} elements when only {len} available",
-                len = data.len()
-            )
-        })
-        .unwrap()
+    data.split_off_mut(..prefix).unwrap_or_else(|| {
+        panic!(
+            "Attempted to slice {prefix} elements when only {len} available",
+            len = data.len()
+        )
+    })
 }
 
 pub(crate) fn try_slice_take_prefix_mut<'t, T>(
