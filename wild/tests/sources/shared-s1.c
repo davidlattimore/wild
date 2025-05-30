8eef3294627dd04826e53d0cb3a628ef1901ec33
lapla-cogito <me@lapla.dev>
2025-05-30 20:54:38 +0900

    update tests

--- wild/tests/sources/shared-s1.c
+++ wild/tests/sources/shared-s1.c
@@ -7,3 +7,6 @@ int call_baz(void) {
 int bar2(void) {
     return 2;
 }
+
+__attribute__((weak)) int foo1 = 3;
+__attribute__((weak)) int get_foo() { return 4; }
--- wild/tests/sources/shared.c
+++ wild/tests/sources/shared.c
@@ -8,14 +8,37 @@
 //#RunEnabled:false
 //#LinkArgs:-shared -z now
 //#Static:false
+// TODO: https://rust-lang.zulipchat.com/#narrow/channel/421156-gsoc/topic/Project.3A.20Improve.20Wild.20linker.20test.20suites/near/521482968
+//#Cross:false
 //#Archive:shared-a1.c,shared-a2.c
 //#Shared:shared-s1.c
 //#DiffIgnore:.dynamic.DT_RELA
 //#DiffIgnore:.dynamic.DT_RELAENT
 //#DiffIgnore:.dynamic.DT_NEEDED
 
+//#Config:symbolic:default
+//#LinkArgs:-shared -z now -Bsymbolic
+//#DiffIgnore:.dynamic.DT_FLAGS.SYMBOLIC
+//#DiffIgnore:.dynamic.DT_SYMBOLIC
+//#DiffIgnore:section.got
+//#DiffIgnore:rel.R_X86_64_PC32.R_X86_64_PLT32
+//#DiffIgnore:rel.extra-opt.R_AARCH64_CALL26.ReplaceWithNop.invalid-shared-object
+
 //#Config:symbolic-functions:default
 //#LinkArgs:-shared -z now -Bsymbolic-functions
+
+//#Config:nosymbolic:default
+//#LinkArgs:-shared -z now -Bno-symbolic
+
+//TODO: Add a test for `-Bsymbolic-non-weak`. Currently, adding such tests causes linker-diff to panic.
+
+//#Config:symbolic-non-weak-functions:default
+//#LinkArgs:-shared -z now -Bsymbolic-non-weak-functions
+//#SkipLinker:ld
+//#EnableLinker:lld
+//#DiffIgnore:section.relro_padding
+//#DiffIgnore:section.got.plt.entsize
+//#DiffIgnore:dynsym.baz.section
 
 int bar1(void);
 int bar2(void);
