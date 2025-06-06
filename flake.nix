a41e6718506547b738e68d4c13d2a90634ccfe63
Ross Smyth <18294397+RossSmyth@users.noreply.github.com>
2025-06-06 15:03:51 -0400

    nix: Support more than just Linux in the flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,10 @@
 {
   inputs = {
     nixpkgs.url = "https://nixos.org/channels/nixos-unstable/nixexprs.tar.xz";
+
+    # TODO: once rust 1.87 (nixos/nixpkgs#407444) hits unstable
+    # and davidlattimore/wild#831 no longer depends on rust nightly,
+    # we should switch to the standard nixpkgs rustPlatform
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -14,50 +18,67 @@
       rust-overlay,
     }:
     let
-      system = "x86_64-linux";
+      forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
 
-      pkgs = import nixpkgs {
-        inherit system;
-        overlays = [ (import rust-overlay) ];
-      };
-      # TODO: once rust 1.87 (nixos/nixpkgs#407444) hits unstable
-      # and davidlattimore/wild#831 no longer depends on rust nightly,
-      # we should switch to the standard nixpkgs rustPlatform
-      rustToolchain = pkgs.rust-bin.beta.latest.minimal;
-      rustPlatform = pkgs.makeRustPlatform {
-        rustc = rustToolchain;
-        cargo = rustToolchain;
-      };
+      common = forAllSystems (system: {
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            self.overlays.default
+            (import rust-overlay)
+          ];
+        };
+
+        rustToolchain = common.${system}.pkgs.rust-bin.beta.latest.minimal;
+        rustPlatform = common.${system}.pkgs.makeRustPlatform {
+          rustc = common.${system}.rustToolchain;
+          cargo = common.${system}.rustToolchain;
+        };
+      });
     in
     {
-      lib.useWildLinker = import ./adapter.nix {
-        inherit pkgs;
-        inherit (pkgs) lib;
-      };
-      packages.${system}.default = pkgs.callPackage ./. { inherit rustPlatform; };
-
-      overlays.default = final: prev: {
-        wild = final.callPackage ./. { inherit rustPlatform; };
-        useWildLinker = import ./adapter.nix {
-          pkgs = final;
-          lib = final.lib;
-        };
-      };
-
-      formatter.${system} = pkgs.nixfmt-tree;
-
-      checks.${system} =
+      packages = forAllSystems (
+        system:
         let
-          pkgs = import nixpkgs {
-            inherit system;
-            overlays = [ self.overlays.default ];
+          inherit (common.${system}) pkgs rustPlatform;
+        in
+        {
+          default = pkgs.callPackage ./. { inherit rustPlatform; };
+        }
+      );
+
+      overlays.default =
+        final: prev:
+        let
+          rustToolchain = (rust-overlay.lib.mkRustBin { } final).beta.latest.minimal;
+          rustPlatform = final.makeRustPlatform {
+            rustc = rustToolchain;
+            cargo = rustToolchain;
           };
+        in
+        {
+          wild = final.callPackage ./. { inherit rustPlatform; };
+          useWildLinker = import ./adapter.nix {
+            pkgs = final;
+            lib = final.lib;
+          };
+        };
+
+      formatter = forAllSystems (system: common.${system}.pkgs.nixfmt-tree);
+
+      checks = forAllSystems (
+        system:
+        let
+          inherit (common.${system}) pkgs rustPlatform;
         in
         {
           wild = self.packages.${system}.default.overrideAttrs { doCheck = true; };
         }
-        // ((pkgs.callPackage ./. { inherit rustPlatform; }).tests);
+        // ((pkgs.callPackage ./. { inherit rustPlatform; }).tests)
+      );
 
-      devShells.${system}.default = pkgs.callPackage ./shell.nix { };
+      devShells = forAllSystems (system: {
+        default = common.${system}.pkgs.callPackage ./shell.nix { };
+      });
     };
 }
