e58d349bc6ecf69e789a5da60301984d720c8c98
David Lattimore <dvdlttmr@gmail.com>
2025-06-14 13:16:54 +1000

    Don't report shlib undefined when writing an shlib

--- wild/tests/sources/shlib-undefined.c
+++ wild/tests/sources/shlib-undefined.c
@@ -2,17 +2,24 @@
 //#Object:runtime.c
 //#Static:false
 //#Shared:shlib-undefined-2.c
-
-//#Config:allow:default
-//#LinkArgs:--allow-shlib-undefined -z now
-//#RunEnabled:false
 //#DiffIgnore:.dynamic.DT_RELA
 //#DiffIgnore:.dynamic.DT_RELAENT
 //#DiffIgnore:.dynamic.DT_NEEDED
 
+//#Config:allow:default
+//#LinkArgs:--allow-shlib-undefined -z now
+//#RunEnabled:false
+
 //#Config:disallow:default
 //#LinkArgs:--no-allow-shlib-undefined
 //#ExpectError:def2
+
+//#Config:shared:default
+//#LinkArgs:-z now -shared
+//#RunEnabled:false
+// TODO: GNU ld sets the entry to _start even though we're writing a shared object. We probably
+// should too.
+//#DiffIgnore:file-header.entry
 
 #include "runtime.h"
 
