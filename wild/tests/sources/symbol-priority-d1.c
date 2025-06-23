c0142b0c05b3dd0a4dde0c03176ab4f46ca85efd
David Lattimore <dvdlttmr@gmail.com>
2025-06-23 16:48:13 +1000

    Improve symbol priority handling and testing
    Issue #826

---
+++ wild/tests/sources/symbol-priority-d1.c
@@ -0,0 +1,7 @@
+__attribute__ ((weak))
+int v1(void) {
+    return 10;
+}
+
+__attribute__ ((weak))
+int v2[2] = {1, 20};
---
+++ wild/tests/sources/symbol-priority.c
@@ -0,0 +1,41 @@
+//#Object:runtime.c
+//#Mode:dynamic
+//#CompSoArgs:-fPIC
+//#Shared:symbol-priority-d1.c
+//#Shared:symbol-priority-d2.c
+//#Shared:symbol-priority-d3.c
+//#Object:symbol-priority-s1.c
+//#Object:symbol-priority-s2.c
+//#Object:symbol-priority-s3.c
+//#DiffIgnore:.dynamic.DT_NEEDED
+//#DiffIgnore:section.got
+
+#include "runtime.h"
+
+// This function is defined weakly in all the files, except the last shared object, which defines it
+// strongly. The definition used should be the first one that isn't from a shared object. It doesn't
+// matter that it's weak since there are no strong definitions in regular objects.
+int v1(void);
+
+// This symbol is defined in all the shared objects. The first shared object defines it weakly as
+// data. The second and third defines it strongly as a function. Because these definitions are from
+// shared objects, weak vs strong doesn't affect which we select, so the first definition should be
+// chosen. We shouldn't error despite having two strong definitions, since both are from shared
+// objects. We have a direct reference to the symbol, so will need a copy relocation. Having data
+// and functions with the same name isn't something we'd expect to see, however it's a useful way to
+// verify that we're selecting the symbol from expected shared object.
+extern int v2[];
+
+void _start(void) {
+    runtime_init();
+
+    if (v1() != 13) {
+        exit_syscall(v1());
+    }
+
+    if (v2[1] != 20) {
+        exit_syscall(5);
+    }
+
+    exit_syscall(42);
+}
