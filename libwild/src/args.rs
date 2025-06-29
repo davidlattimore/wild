9a3c9b62a327b1dcd26637247deb9c6d5ebc2174
Martin Liška <martin.liska@hey.com>
2025-06-29 14:53:25 +0200

    args: add one more use of get_next_argument (#959)

--- libwild/src/args.rs
+++ libwild/src/args.rs
@@ -583,8 +583,7 @@ pub(crate) fn parse<F: Fn() -> I, S: AsRef<str>, I: Iterator<Item = S>>(input: F
         } else if let Some(rest) = long_arg_split_prefix("rpath=") {
             append_rpath(&mut args.rpath, rest);
         } else if arg == "-R" {
-            let value = input.next().context("Missing argument to -R")?;
-            handle_r_option(value.as_ref());
+            handle_r_option(get_next_argument(arg)?.as_ref());
         } else if let Some(rest) = arg.strip_prefix("-R") {
             handle_r_option(rest);
         } else if long_arg_eq("no-string-merge") {
