a24ef1330b7a20bf05190019614d8cef6e3a689d
lapla-cogito <me@lapla.dev>
2025-07-02 12:34:26 +0900

    allow skipping of mold tests

--- wild/tests/external_tests/mod.rs
+++ wild/tests/external_tests/mod.rs
@@ -1,2 +1,18 @@
 #[cfg(feature = "mold_tests")]
 mod mold_tests;
+
+#[allow(unused)]
+fn should_not_ignore_tests(external_test: &str) -> bool {
+    let wild_ignore_skip: Option<Vec<String>> =
+        std::env::var("WILD_IGNORE_SKIP").ok().map(|test_suites| {
+            test_suites
+                .split(',')
+                .map(|suite| suite.trim().to_string())
+                .filter(|suite| !suite.is_empty())
+                .collect()
+        });
+
+    wild_ignore_skip.is_some_and(|tests| {
+        tests.contains(&external_test.to_string()) || tests.contains(&"all".to_string())
+    })
+}
