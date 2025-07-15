774ed875b61ac6f4dde19c72c862f5d5e6470d98
Mateusz Mikuła <oss@mateuszmikula.dev>
2025-07-15 20:56:14 +0200

    remove clang-format markers

--- wild/tests/sources/export-dynamic.c
+++ wild/tests/sources/export-dynamic.c
@@ -1,60 +1,59 @@
-// clang-format off
-// Adding shared libraries should result in dynamic executables when unspecified: #836
-//#Config:shared-input
-//#LinkArgs:-z now --export-dynamic
-//#ExpectDynSym:foo
-//#Shared:empty.c
-//#Mode:unspecified
-//#Cross:false
+// Adding shared libraries should result in dynamic executables when
+// unspecified: #836
+// #Config:shared-input
+// #LinkArgs:-z now --export-dynamic
+// #ExpectDynSym:foo
+// #Shared:empty.c
+// #Mode:unspecified
+// #Cross:false
 // We're linking different .so files, so this is expected.
-//#DiffIgnore:.dynamic.DT_NEEDED
+// #DiffIgnore:.dynamic.DT_NEEDED
 // TODO: Wild probably should set dynamic linker here
-//#EnableLinker:lld
-//#RunEnabled:false
+// #EnableLinker:lld
+// #RunEnabled:false
 
 // Do not export symbols for static executables: #836
-//#Config:static-exe
-//#Mode:static
-//#LinkArgs:-z now --export-dynamic
-//#RunEnabled:false
-//#DoesNotContain:.dynamic
+// #Config:static-exe
+// #Mode:static
+// #LinkArgs:-z now --export-dynamic
+// #RunEnabled:false
+// #DoesNotContain:.dynamic
 
-//#Config:select-symbols
-//#LinkArgs:-z now --export-dynamic-symbol bar --export-dynamic-symbol baz
-//#ExpectDynSym:bar
-//#ExpectDynSym:baz
-//#Shared:empty.c
-//#Mode:dynamic
-//#Cross:false
-// We're linking different .so files, so this is expected.
-//#DiffIgnore:.dynamic.DT_NEEDED
-//#EnableLinker:lld
-//#RunEnabled:false
+// #Config:select-symbols
+// #LinkArgs:-z now --export-dynamic-symbol bar --export-dynamic-symbol baz
+// #ExpectDynSym:bar
+// #ExpectDynSym:baz
+// #Shared:empty.c
+// #Mode:dynamic
+// #Cross:false
+//  We're linking different .so files, so this is expected.
+// #DiffIgnore:.dynamic.DT_NEEDED
+// #EnableLinker:lld
+// #RunEnabled:false
 
-//#Config:select-symbols-list
-//#LinkArgs:-z now --export-dynamic-symbol-list ./export-dynamic.def
-//#ExpectDynSym:foo
-//#ExpectDynSym:baz
-//#Shared:empty.c
-//#Mode:dynamic
-//#Cross:false
-// We're linking different .so files, so this is expected.
-//#DiffIgnore:.dynamic.DT_NEEDED
-//#RunEnabled:false
+// #Config:select-symbols-list
+// #LinkArgs:-z now --export-dynamic-symbol-list ./export-dynamic.def
+// #ExpectDynSym:foo
+// #ExpectDynSym:baz
+// #Shared:empty.c
+// #Mode:dynamic
+// #Cross:false
+//  We're linking different .so files, so this is expected.
+// #DiffIgnore:.dynamic.DT_NEEDED
+// #RunEnabled:false
 
-//#Config:dynamic-symbols-list
-//#LinkArgs:-z now -shared --dynamic-list ./export-dynamic.def
-//#ExpectDynSym:foo
-//#ExpectDynSym:baz
-//#Shared:empty.c
-//#Mode:dynamic
-//#Cross:false
-// We're linking different .so files, so this is expected.
-//#DiffIgnore:.dynamic.DT_NEEDED
-//#DiffIgnore:.dynamic.DT_RELA*
-//#DiffIgnore:file-header.entry
-//#RunEnabled:false
-// clang-format on
+// #Config:dynamic-symbols-list
+// #LinkArgs:-z now -shared --dynamic-list ./export-dynamic.def
+// #ExpectDynSym:foo
+// #ExpectDynSym:baz
+// #Shared:empty.c
+// #Mode:dynamic
+// #Cross:false
+//  We're linking different .so files, so this is expected.
+// #DiffIgnore:.dynamic.DT_NEEDED
+// #DiffIgnore:.dynamic.DT_RELA*
+// #DiffIgnore:file-header.entry
+// #RunEnabled:false
 
 void foo(void) {};
 void bar(void) {};
