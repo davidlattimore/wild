ea83197ce7f53f5bb08a612183df55b34840b2ef
lapla-cogito <me@lapla.dev>
2025-07-24 10:58:56 +0900

    detect tests that can be removed from the skip list

--- wild/tests/external_tests/mod.rs
+++ wild/tests/external_tests/mod.rs
@@ -1,6 +1,12 @@
 #[cfg(feature = "mold_tests")]
 mod mold_tests;
 
+use crate::Result;
+use std::env;
+use std::path::Path;
+use std::process::Command;
+use std::process::Output;
+
 #[allow(unused)]
 fn should_not_ignore_tests(external_test: &str) -> bool {
     let wild_ignore_skip: Option<Vec<String>> =
@@ -15,4 +21,19 @@ fn should_not_ignore_tests(external_test: &str) -> bool {
     wild_ignore_skip.is_some_and(|tests| {
         tests.contains(&external_test.to_string()) || tests.contains(&"all".to_string())
     })
+}
+
+#[allow(unused)]
+fn run_external_test(external_test: &Path) -> Result<Output> {
+    let path = env::var("PATH")?;
+    let current_dir = env::current_dir()?;
+    let wild_dir = current_dir.parent().unwrap().join("fakes-debug");
+
+    Command::new("bash")
+        .current_dir("../fakes-debug")
+        .arg("-c")
+        .arg(format!("{} 2>&1", external_test.display()))
+        .env("PATH", format!("{wild_dir:?}:{path}"))
+        .output()
+        .map_err(Into::into)
 }
