7c247d880a5052747286a818c2b7d7dc5c912045
lapla <me@lapla.dev>
2025-07-31 14:53:34 +0900

    Implement `--allow-multiple-definition` and `-z muldef` (#1015)

--- wild/tests/sources/duplicate_strong_symbols.c
+++ wild/tests/sources/duplicate_strong_symbols.c
@@ -3,6 +3,16 @@
 //#Object:duplicate_strong_symbols2.c
 //#ExpectError:Duplicate symbols
 
+//#Config:allow-multiple-definition
+//#RunEnabled:false
+//#Object:duplicate_strong_symbols2.c
+//#LinkArgs:--allow-multiple-definition
+
+//#Config:z-muldefs
+//#RunEnabled:false
+//#Object:duplicate_strong_symbols2.c
+//#LinkArgs:-z muldefs
+
 int test_func(void) {
     return 0;
 }
