davidlattimore/wild

commit 7a551dc443a3ff5f2edbcb2f7e8f647af6d56a94

Support `-l:<name.ext>` args

libwild/src/args.rs
@@ -338,8 +338,13 @@ pub(crate) fn parse<S: AsRef<str>, I: Iterator<Item = S>>(mut input: I) -> Resul
                 args.lib_search_path.push(handle_sysroot(Path::new(rest)));
             }
         } else if let Some(rest) = arg.strip_prefix("-l") {
+            let spec = if let Some(stripped) = rest.strip_prefix(':') {
+                InputSpec::File(Box::from(Path::new(stripped)))
+            } else {
+                InputSpec::Lib(Box::from(rest))
+            };
             args.inputs.push(Input {
-                spec: InputSpec::Lib(Box::from(rest)),
+                spec,
                 search_first: None,
                 modifiers: *modifier_stack.last().unwrap(),
             });
@@ -953,6 +958,7 @@ mod tests {
         "--sysroot=/usr/aarch64-linux-gnu",
         "--demangle",
         "--no-demangle",
+        "-l:lib85caec4suo0pxg06jm2ma7b0o.so",
     ];
 
     #[track_caller]
@@ -991,6 +997,10 @@ mod tests {
             args.sysroot,
             Some(Box::from(Path::new("/usr/aarch64-linux-gnu")))
         );
+        assert!(args.inputs.iter().any(|i| match &i.spec {
+            InputSpec::File(f) => f.as_ref() == Path::new("lib85caec4suo0pxg06jm2ma7b0o.so"),
+            InputSpec::Lib(_) => false,
+        }));
     }
 
     #[test]

libwild/src/input_data.rs
@@ -221,7 +221,7 @@ impl Input {
     fn path(&self, args: &Args) -> Result<InputPath> {
         match &self.spec {
             InputSpec::File(p) => {
-                if self.search_first.is_some() {
+                if self.search_first.is_some() || p.parent() == Some(Path::new("")) {
                     if let Some(absolute) = search_for_file(
                         &args.lib_search_path,
                         self.search_first.as_ref(),

commit d16882f68d3c0ce7502e90a9d5db7446f01da462

Do not lookup files from args in search paths
Fixes a regression from #610 that manifests in cups source code.
Minimised reproducer:
```sh
$ mkdir bad_foo
$ touch bad_foo/foo.o
$ echo 'int main() { return 0; }' > main.c
$ gcc -c main.c -o foo.o
❯ gcc -B ~/Projects/wild/fakes-debug/ -pie foo.o -Lbad_foo/
wild: error: Undefined symbol main, referenced by
    /usr/lib/gcc/x86_64-pc-linux-gnu/15.2.1/../../../../lib/Scrt1.o
collect2: error: ld returned 255 exit status
```
The problem was due to `bad_foo/foo.o` being preferred over `./foo.o`.

libwild/src/args.rs
@@ -194,8 +194,10 @@ pub(crate) struct Input {
 
 #[derive(Debug, Eq, PartialEq)]
 pub(crate) enum InputSpec {
+    /// Path (possibly just a filename) to the file.
     File(Box<Path>),
-    Lib(Box<str>),
+    /// Name of the library and whether it already has suffix and prefix.
+    Lib(Box<str>, bool),
 }
 
 #[derive(Debug, Eq, PartialEq)]
@@ -1198,7 +1200,7 @@ fn setup_argument_parser() -> ArgumentParser {
             "libname",
             "Link with library libname.so or libname.a",
             |args, modifier_stack, value| {
-                let spec = InputSpec::Lib(Box::from(value));
+                let spec = InputSpec::Lib(Box::from(value), false);
                 args.inputs.push(Input {
                     spec,
                     search_first: None,
@@ -1209,9 +1211,9 @@ fn setup_argument_parser() -> ArgumentParser {
         )
         .execute(|args, modifier_stack, value| {
             let spec = if let Some(stripped) = value.strip_prefix(':') {
-                InputSpec::File(Box::from(Path::new(stripped)))
+                InputSpec::Lib(Box::from(stripped), true)
             } else {
-                InputSpec::Lib(Box::from(value))
+                InputSpec::Lib(Box::from(value), false)
             };
             args.inputs.push(Input {
                 spec,
@@ -2360,8 +2362,8 @@ mod tests {
             args.inputs
                 .iter()
                 .filter_map(|i| match &i.spec {
-                    InputSpec::File(_) => None,
-                    InputSpec::Lib(lib_name) => Some(lib_name.as_ref()),
+                    InputSpec::File(_) | InputSpec::Lib(_, true) => None,
+                    InputSpec::Lib(lib_name, false) => Some(lib_name.as_ref()),
                 })
                 .collect_vec(),
             &["gcc_s", "util", "rt", "pthread", "m", "dl", "c"]
@@ -2370,7 +2372,7 @@ mod tests {
         assert_contains(&args.lib_search_path, "/usr/lib");
         assert!(!args.inputs.iter().any(|i| match &i.spec {
             InputSpec::File(f) => f.as_ref() == Path::new("/usr/bin/ld"),
-            InputSpec::Lib(_) => false,
+            InputSpec::Lib(_, _) => false,
         }));
         assert_eq!(
             args.version_script_path,
@@ -2384,8 +2386,8 @@ mod tests {
             Some(Box::from(Path::new("/usr/aarch64-linux-gnu")))
         );
         assert!(args.inputs.iter().any(|i| match &i.spec {
-            InputSpec::File(f) => f.as_ref() == Path::new("lib85caec4suo0pxg06jm2ma7b0o.so"),
-            InputSpec::Lib(_) => false,
+            InputSpec::File(_) | InputSpec::Lib(_, false) => false,
+            InputSpec::Lib(lib, true) => lib.as_ref() == "lib85caec4suo0pxg06jm2ma7b0o.so",
         }));
         assert_eq!(args.rpath.as_deref(), Some("foo/:bar/:baz:somewhere"));
     }

libwild/src/input_data.rs
@@ -623,7 +623,7 @@ impl Input {
     fn path(&self, args: &Args) -> Result<InputPath> {
         match &self.spec {
             InputSpec::File(p) => {
-                if (self.search_first.is_some() || p.parent() == Some(Path::new("")))
+                if self.search_first.is_some()
                     && let Some(path) = search_for_file(
                         &args.lib_search_path,
                         self.search_first.as_ref(),
@@ -640,9 +640,13 @@ impl Input {
                     original: p.as_ref().to_owned(),
                 })
             }
-            InputSpec::Lib(lib_name) => {
+            InputSpec::Lib(lib_name, literal_name) => {
                 if self.modifiers.allow_shared {
-                    let filename = format!("lib{lib_name}.so");
+                    let filename = if *literal_name {
+                        lib_name.to_string()
+                    } else {
+                        format!("lib{lib_name}.so")
+                    };
                     if let Some(path) = search_for_file(
                         &args.lib_search_path,
                         self.search_first.as_ref(),
@@ -654,7 +658,11 @@ impl Input {
                         });
                     }
                 }
-                let filename = format!("lib{lib_name}.a");
+                let filename = if *literal_name {
+                    lib_name.to_string()
+                } else {
+                    format!("lib{lib_name}.a")
+                };
                 if let Some(path) =
                     search_for_file(&args.lib_search_path, self.search_first.as_ref(), &filename)
                 {

libwild/src/linker_script.rs
@@ -346,7 +346,7 @@ fn foreach_input(
         match command {
             Command::Arg(arg) => {
                 let spec = if let Some(lib_name) = arg.strip_prefix("-l".as_bytes()) {
-                    InputSpec::Lib(Box::from(to_str(lib_name)?))
+                    InputSpec::Lib(Box::from(to_str(lib_name)?), false)
                 } else {
                     InputSpec::File(Box::from(Path::new(to_str(arg)?)))
                 };
@@ -423,7 +423,7 @@ mod tests {
             inputs.into_iter().map(|i| i.spec),
             [
                 InputSpec::File(Box::from(Path::new("libgcc_s.so.1"))),
-                InputSpec::Lib(Box::from("gcc")),
+                InputSpec::Lib(Box::from("gcc"), false),
             ],
         );
 