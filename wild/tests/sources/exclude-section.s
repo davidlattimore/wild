d57edb5dbc731d220ff3929252c29161d9fcfb66
Rémy Rakic <remy.rakic+github@gmail.com>
2025-09-10 19:57:15 +0000

    add excluded section unit test

---
+++ wild/tests/sources/exclude-section.s
@@ -0,0 +1,14 @@
+/* For some reason, GAS on riscv64 does not support '//' comments.
+//#Mode:dynamic
+//#LinkArgs:-shared --no-gc-sections -z now
+//#DiffIgnore:section.got
+//#DiffIgnore:segment.LOAD.RX.alignment
+//#Contains:ShouldBeKept
+//#DoesNotContain:ShouldBeExcluded
+*/
+
+.section .keep
+.ascii "ShouldBeKept\0"
+
+.section .exclude, "e"
+.ascii "ShouldBeExcluded\0"
