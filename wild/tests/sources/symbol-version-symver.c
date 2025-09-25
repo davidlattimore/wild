00d25c4be38698dbcf2edad33459a750dcfaa8a9
Martin Liska <martin.liska@hey.com>
2025-09-25 19:25:56 +0200

    relax version_d linker-diff for the first base verdef

--- wild/tests/sources/symbol-version-symver.c
+++ wild/tests/sources/symbol-version-symver.c
@@ -1,13 +1,11 @@
-//#AbstractConfig:default
-
-//#Config:shared-lib:default
-//#SkipLinker:ld
+//#Config:default
+//#DiffIgnore:.dynamic.DT_RELAENT
+//#DiffIgnore:.dynamic.DT_RELA
 //#RunEnabled:false
-//#LinkArgs:--shared --version-script=./symbol-versions-script.map
-//#ExpectError: symbol version definition
+//#LinkArgs:--shared --version-script=./symbol-versions-script.map -z now
 
-__asm__(".symver foo,xyz@VER_1");
-__asm__(".symver bar,xyz@@VER_2");
+__asm__(".symver foo_impl,foo@VER_1.0");
+__asm__(".symver bar_impl,bar@@VER_1.0");
 
-void foo(void) { __builtin_printf("foo\n"); }
-void bar(void) { __builtin_printf("bar\n"); }
+void foo_impl(void) { __builtin_printf("foo\n"); }
+void bar_impl(void) { __builtin_printf("bar\n"); }
