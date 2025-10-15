71bdd4de53063e726763ea13a497878fd03be992
David Lattimore <dvdlttmr@gmail.com>
2025-10-16 10:57:58 +1100

    Fix a test that was expecting dynamic linking, but wasn't (#1216)
    Mode:dynamic adds the --dynamic-linker flag, which used to cause wild to
    dynamically link. Since this is no longer the case, this test wasn't actually
    dynamically linking. We add a check to the integration test framework that when
    we specify `Mode:dynamic` that we end up with .dynsym.

---
+++ wild/tests/sources/force-dynamic-linking.c
@@ -0,0 +1,5 @@
+// The contents of this file isn't really important. We link against it as a
+// shared object as a way to force the linker to produce a dynamically linked
+// executable.
+
+int x(void) {}
