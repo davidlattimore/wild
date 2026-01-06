cca7a5b3cbc13f4c384adebcc824d24bb767750a
lapla <me@lapla.dev>
2026-01-06 09:48:15 +0900

    feat: Support expressions in `--defsym` and linker script symbol assignment (#1418)
    Previously, symbol assignments in the `--defsym` option or in linker
    scripts could only contain absolute addresses or symbol names on the
    right-hand side of the assignment. However, other existing linkers allow
    well-formed expressions on the right-hand side.
    
    The following statements are from the description of the `--defsym`
    option in the GNU ld man page:
    
    > A limited form of arithmetic is supported for the expression in this
    context: you may specify a hexadecimal constant or the name of an
    existing symbol, or use "+" and "-" to add or subtract hexadecimal
    constants or symbols. For more complex expressions, consider using the
    linker command language from a script. Note: there should be no
    whitespace between the symbol, the equals sign ("="), and the
    expression.
    
    Additionally, documentation on symbol assignments in linker scripts can
    be found here:
    
    https://sourceware.org/binutils/docs-2.17/ld/Simple-Assignments.html#Simple-Assignments
    
    While this documentation mentions expressions such as `symbol +=
    expression`, it is unclear whether these are widely used in practice.
    Therefore, I plan to implement this support in a separate PR.

--- wild/tests/sources/defsym.c
+++ wild/tests/sources/defsym.c
@@ -9,6 +9,18 @@
 //#LinkArgs:--defsym=bar=0x12345678
 //#ExpectSym:bar address=0x12345678
 
+//#Config:address_expr:default
+//#LinkArgs:--defsym=bar=0x1111+0x2222
+//#ExpectSym:bar address=0x3333
+
+//#Config:symbol_expr:default
+//#LinkArgs:--defsym=bar=0x1111 --defsym=baz=bar+0x2222
+//#ExpectSym:baz address=0x3333
+
+//#Config:symbol_expr2:default
+//#LinkArgs:--defsym=bar=0x2222 --defsym=baz=-0x1111+bar
+//#ExpectSym:baz address=0x1111
+
 //#Config:not_exist:default
 //#LinkArgs:--defsym=bar=notexist
 //#SkipLinker:ld
