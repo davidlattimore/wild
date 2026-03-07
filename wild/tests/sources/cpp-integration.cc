e463c060fb4fd834d8529fd4d3af32a4dbda90e1
Mateusz Mikuła <oss@mateuszmikula.dev>
2026-03-07 20:44:04 +0100

    test: ignore it, whatever it is

--- wild/tests/sources/cpp-integration.cc
+++ wild/tests/sources/cpp-integration.cc
@@ -34,8 +34,9 @@
 //#DiffIgnore:rel.match_failed.R_X86_64_PLT32
 //#DiffIgnore:literal-byte-mismatch
 //#DiffIgnore:rel.extra-opt.R_X86_64_GOTPCRELX.JmpIndirectToRelative.static-non-pie
-//#DiffIgnore:rel.extra-opt.R_X86_64_REX_GOTPCRELX.RexMovIndirectToAbsolute.static-non-pie
 //#DiffIgnore:rel.extra-opt.R_X86_64_REX_GOTPCRELX.RexCmpIndirectToAbsolute.static-non-pie
+//#DiffIgnore:rel.extra-opt.R_X86_64_REX_GOTPCRELX.RexMovIndirectToAbsolute.static-non-pie
+//#DiffIgnore:rel.extra-opt.R_X86_64_REX_GOTPCRELX.RexMovIndirectToAbsolute(3).static-non-pie
 //#DiffIgnore:section.rela.plt.link
 //#Arch: x86_64
 
