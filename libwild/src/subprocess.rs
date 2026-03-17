88bbd902e525c1c439acfbf586ff6bb97aab077c
David Lattimore <dvdlttmr@gmail.com>
2026-03-18 07:44:03 +1100

    refactor: Separate thread pool / jobserver stuff a bit from Args (#1705)

--- libwild/src/subprocess.rs
+++ libwild/src/subprocess.rs
@@ -29,7 +29,7 @@ pub unsafe fn run_in_subprocess(args: Args<ElfArgs>) -> ! {
     std::process::exit(exit_code);
 }
 
-fn subprocess_result(args: Args<ElfArgs>) -> Result<i32> {
+fn subprocess_result(mut args: Args<ElfArgs>) -> Result<i32> {
     let mut fds: [c_int; 2] = [0; 2];
     // create the pipe used to communicate between the parent and child processes - exit on failure
     make_pipe(&mut fds).context("make_pipe")?;
@@ -42,9 +42,9 @@ fn subprocess_result(args: Args<ElfArgs>) -> Result<i32> {
             // Fork success in child - Run linker in this process.
 
             crate::setup_tracing(&args)?;
-            let args = args.activate_thread_pool()?;
+            let thread_pool = args.activate_thread_pool()?;
             let linker = crate::Linker::new();
-            let _outputs = linker.run(&args)?;
+            let _outputs = linker.run(&args, &thread_pool)?;
             crate::timing::finalise_perfetto_trace()?;
             inform_parent_done(&fds);
             Ok(0)
