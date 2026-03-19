73bb142eb6abfebe4a7c5ddb6734371285d09c4d
Mateusz Mikuła <oss@mateuszmikula.dev>
2026-03-19 23:06:00 +0100

    move glibc special symbol writing to the end
    Its location doesn't matter, GNU ld just reversely sorts the versions
    within each verneed.

--- libwild/src/elf_writer.rs
+++ libwild/src/elf_writer.rs
@@ -5076,28 +5076,6 @@ fn write_dynamic_file<'data, A: Arch<Platform = Elf>>(
             .version_writer
             .take_auxes(verneed_info.version_count)?;
         let mut aux_index = 0;
-
-        // Emit GLIBC_ABI_DT_RELR as the first .gnu.version_r entry when using -z
-        // pack-relative-relocs.
-        if verneed_info.has_dt_relr_version {
-            let name_offset = table_writer
-                .dynsym_writer
-                .strtab_writer
-                .write_str(GLIBC_ABI_DT_RELR);
-            let sysv_name_hash = object::elf::hash(GLIBC_ABI_DT_RELR);
-            let aux_out = auxes
-                .get_mut(aux_index)
-                .context("Insufficient vernaux allocation for GLIBC_ABI_DT_RELR")?;
-            aux_out.vna_next.set(e, size_of::<Vernaux>() as u32);
-            aux_out
-                .vna_other
-                .set(e, layout.non_addressable_counts.final_version_index);
-            aux_out.vna_name.set(e, name_offset);
-            aux_out.vna_hash.set(e, sysv_name_hash);
-            aux_out.vna_flags.set(e, 0);
-            aux_index += 1;
-        }
-
         while let Some((verdef, mut aux_iterator)) = verdefs.next()? {
             let input_version = verdef.vd_ndx.get(e);
             let flags = verdef.vd_flags.get(e);
@@ -5149,6 +5127,24 @@ fn write_dynamic_file<'data, A: Arch<Platform = Elf>>(
                 aux_out.vna_flags.set(e, 0);
                 aux_index += 1;
             }
+        }
+        if verneed_info.has_dt_relr_version {
+            let name_offset = table_writer
+                .dynsym_writer
+                .strtab_writer
+                .write_str(GLIBC_ABI_DT_RELR);
+            let sysv_name_hash = object::elf::hash(GLIBC_ABI_DT_RELR);
+            let aux_out = auxes
+                .get_mut(aux_index)
+                .context("Insufficient vernaux allocation for GLIBC_ABI_DT_RELR")?;
+            aux_out.vna_next.set(e, 0);
+            aux_out
+                .vna_other
+                .set(e, layout.non_addressable_counts.final_version_index);
+            aux_out.vna_name.set(e, name_offset);
+            aux_out.vna_hash.set(e, sysv_name_hash);
+            aux_out.vna_flags.set(e, 0);
+            aux_index += 1;
         }
         debug_assert_eq!(aux_index, auxes.len());
     }
