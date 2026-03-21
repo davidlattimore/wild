e80b11618c40029b3539fb9b618aa64caeff24e3
David Lattimore <dvdlttmr@gmail.com>
2026-03-22 08:25:03 +1100

    fix: Don't report undefined symbols when writing executables with -z undefs (#1741)

--- wild/tests/sources/undefined_symbols.c
+++ wild/tests/sources/undefined_symbols.c
@@ -1,18 +1,29 @@
 //#AbstractConfig:default
+//#EnableLinker:lld
+//#DiffIgnore:.dynamic.DT_RELA*
+//#DiffIgnore:file-header.entry
 
 //#Config:shared-lib:default
 //#LinkArgs:--shared -z now
 //#RunEnabled:false
-//#DiffIgnore:.dynamic.DT_RELA*
-//#DiffIgnore:file-header.entry
 
 //#Config:no-undefined:default
 //#LinkArgs:--shared --no-undefined
-//#ExpectError:  undefined_strong
+//#ExpectError:undefined_strong
+
+//#Config:z-defs:default
+//#LinkArgs:-z defs
+//#ExpectError:undefined_strong
+
+//#Config:z-undefs:default
+//#LinkArgs:-z undefs
+//#RunEnabled:false
+// GNU ld (2.45) hits an assertion failure for this test
+//#SkipArch:loongarch64
 
 //#Config:executable:default
 //#CompArgs:-g
-//#ExpectError:  undefined_strong
+//#ExpectError:undefined_strong
 //#ExpectError:undefined_symbols.c
 
 int undefined_strong();
