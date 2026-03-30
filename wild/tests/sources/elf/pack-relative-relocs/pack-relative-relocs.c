837a214ff7a0df206181d705c2e1c91a3ca4527a
Mateusz Mikuła <oss@mateuszmikula.dev>
2026-03-30 22:59:06 +0200

    test: extend with constructors

--- wild/tests/sources/elf/pack-relative-relocs/pack-relative-relocs.c
+++ wild/tests/sources/elf/pack-relative-relocs/pack-relative-relocs.c
@@ -1,4 +1,5 @@
 //#Config:default
+//#Object:init.c
 //#Shared:runtime.c
 //#LinkArgs:-pie -z now -z pack-relative-relocs
 // We're linking different .so files, so this is expected.
@@ -7,9 +8,19 @@
 //#RequiresGlibc:true
 //#Mode:dynamic
 
+#include "init.h"
 #include "runtime.h"
 
 int target = 42;
+
+int foo = 0;
+short bar = 0;
+char baz = 0;
+
+__attribute__((constructor)) static void ctor_foo(void) { foo = 1; }
+__attribute__((constructor)) static void ctor_bar(void) { bar = 2; }
+__attribute__((constructor)) static void ctor_baz(void) { baz = 3; }
+__attribute__((destructor)) static void dtor_foo(void) { foo = 0; }
 
 int* aligned_ptr = &target;
 
@@ -31,8 +42,12 @@ struct __attribute__((packed)) {
 
 void _start(void) {
   runtime_init();
+  call_init_functions();
   if (*aligned_ptr != target) exit_syscall(0);
   if (*unaligned_ptr_odd.foo != target) exit_syscall(1);
   if (*unaligned_ptr_even.foo != target) exit_syscall(2);
+  if (foo != 1) exit_syscall(3);
+  if (bar != 2) exit_syscall(4);
+  if (baz != 3) exit_syscall(5);
   exit_syscall(target);
 }
