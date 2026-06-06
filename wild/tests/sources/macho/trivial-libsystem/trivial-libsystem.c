12856b07f257da7075f99dcb90c5f6d5f04f60da
Martin Liska <martin.liska@hey.com>
2026-06-06 15:50:42 +0200

    add test-case for linking against libSystem

---
+++ wild/tests/sources/macho/trivial-libsystem/trivial-libsystem.c
@@ -0,0 +1,23 @@
+//#TestUpdateInPlace:true
+//#DiffIgnore:section.__const
+//#DiffIgnore:section.__unwind_info
+//#LinkerDriver:clang
+//#ExpectWarningWild:Fat object file is not supported yet
+//#SkipLinker:lld
+
+#include <stdio.h>
+#include <string.h>
+
+int value = 121;
+
+int main() {
+  char buf[128];
+  sprintf(buf, "Hello %s: %d", "world", value);
+  printf("buf: '%s'\n", buf);
+
+  unsigned char csum = 0;
+  for (int i = 0; i < strlen(buf); ++i) {
+    csum += buf[i];
+  }
+  return csum;
+}
