664b40c6a54f189b05545a9033721edc582985a6
Martin Liska <martin.liska@hey.com>
2026-06-14 14:32:50 +0200

    add test-case

---
+++ wild/tests/sources/macho/trivial-cpp/trivial-cpp.cc
@@ -0,0 +1,15 @@
+//#LinkerDriver:clang++
+//#ExpectWarningWild:Fat object file is not supported yet
+
+#include <iostream>
+
+struct Foo {
+  static int foo() {
+    return 42;
+  }
+};
+
+int main() {
+  std::cout << "hello world\n" << std::endl;
+  return Foo::foo();
+}
