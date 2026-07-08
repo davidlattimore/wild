ad2d8675b037785303f3e6eea49ee5b8c59951ce
Vishruth Thimmaiah <vishruththimmaiah@gmail.com>
2026-07-08 11:17:07 +0530

    tests: check for weak symbols
    Signed-off-by: Vishruth Thimmaiah <vishruththimmaiah@gmail.com>

--- wild/tests/sources/elf/linker-script-assert-pass/linker-script-assert-pass.c
+++ wild/tests/sources/elf/linker-script-assert-pass/linker-script-assert-pass.c
@@ -4,3 +4,7 @@
 void _start() {}
 
 int symbol4 = 0x4000;
+int symbol6 __attribute__((weak));
+int symbol7 __attribute__((weak)) = 0x7000;
+extern int symbol8 __attribute__((weak));
+extern int symbol9;
