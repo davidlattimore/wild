625005abb17108263f7b2ef43941fbd064e54bcb
Vishruth Thimmaiah <vishruththimmaiah@gmail.com>
2026-07-10 05:15:00 +0530

    tests: verify symbols are defined in symbol table
    Signed-off-by: Vishruth Thimmaiah <vishruththimmaiah@gmail.com>

--- wild/tests/sources/elf/linker-script-assert-pass/linker-script-assert-pass.c
+++ wild/tests/sources/elf/linker-script-assert-pass/linker-script-assert-pass.c
@@ -1,10 +1,15 @@
 //#LinkArgs:-T ./linker-script-assert-pass.ld
 //#RunEnabled:false
 //#DiffEnabled:false
-void _start() {}
+//#Object:runtime.c
+// RISC-V: BFD complains about missing __global_pointer$ (defined in the default linker script)
+//#SkipArch:riscv64
+
+#include "../common/runtime.h"
 
 int symbol4 = 0x4000;
 int symbol6 __attribute__((weak));
 int symbol7 __attribute__((weak)) = 0x7000;
 extern int symbol8 __attribute__((weak));
-extern int symbol9;
+
+void _start() { exit_syscall(symbol4 + symbol6 + symbol7 + symbol8); }
