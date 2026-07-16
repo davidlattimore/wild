5aaa73ca4eda5cba6d5f4fb9fec73f3358c7ae22
Martin Liska <martin.liska@hey.com>
2026-07-16 17:07:13 +0200

    start using pastey (instead of unmaintained paste crate)

--- linker-utils/src/elf.rs
+++ linker-utils/src/elf.rs
@@ -10,7 +10,7 @@ macro_rules! const_or_literal {
         $name($value)
     };
     ($name:ident $prefix:ident $const:ident) => {
-        ::paste::paste! {
+        ::pastey::paste! {
             ::object::elf::[<$prefix _ $const>]
         }
     };
