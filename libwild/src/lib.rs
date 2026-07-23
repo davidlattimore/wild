002865b1d1ad53830d65a059fd06feecae25b5ac
Martin Liska <martin.liska@hey.com>
2026-07-23 12:58:47 +0200

    comment better Linker::run function

--- libwild/src/lib.rs
+++ libwild/src/lib.rs
@@ -208,6 +208,12 @@ impl Linker {
     /// Runs the linker. The returned value isn't useful for anything, but is somewhat expensive to
     /// drop, so we leave it up to the caller to decide when to drop it. At the point at which we
     /// return, the output file should be usable.
+    ///
+    /// This method runs in whatever Rayon thread pool is currently installed. If no thread pool is
+    /// installed, then Rayon's default global thread pool will be created and used. In either case,
+    /// the value of `--threads` in `args` has no effect. If you want a thread pool configured from
+    /// the supplied arguments, use the top-level [`run`] function instead, which creates such a
+    /// pool, runs the linker in it, then tears it down together with other associated resources.
     pub fn run<'layout_inputs>(
         &'layout_inputs self,
         args: &'layout_inputs Args,
