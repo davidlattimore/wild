e9476c0f4560f993f29a36f95844650ab75ea0fc
Mateusz Mikuła <oss@mateuszmikula.dev>
2026-07-25 15:01:05 +0200

    fix: make SAVE_DIR's run-with work with Cargo again

--- libwild/src/save-dir-prelude.sh
+++ libwild/src/save-dir-prelude.sh
@@ -3,5 +3,22 @@ D=$(dirname $BASH_SOURCE)
 if [ -z "$OUT" ]; then
   OUT=$D/bin${S}
 fi
-L="$1"
-shift
+L=()
+has_double_dash=false
+for arg in "$@"; do
+  if [[ "$arg" == "--" ]]; then
+    has_double_dash=true
+    break
+  fi
+done
+if $has_double_dash; then
+  for arg in "$@"; do
+    L+=("$arg")
+    [[ "$arg" == "--" ]] && break
+  done
+else
+  [[ $# -gt 0 ]] && L=("$1")
+fi
+for ((i = 0; i < ${#L[@]}; i++)); do
+  shift
+done
