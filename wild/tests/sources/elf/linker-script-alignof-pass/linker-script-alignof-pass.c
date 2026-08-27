2d2df3483ed86ed4e1f90ebbe4b801ba80f73525
David Lattimore <dvdlttmr@gmail.com>
2026-08-27 10:42:42 +1000

    test: Verify that linker script ALIGN is correctly applied

--- wild/tests/sources/elf/linker-script-alignof-pass/linker-script-alignof-pass.c
+++ wild/tests/sources/elf/linker-script-alignof-pass/linker-script-alignof-pass.c
@@ -1,6 +1,7 @@
 //#LinkArgs:-T ./linker-script-alignof-pass.ld
 //#RunEnabled:false
 //#DiffEnabled:false
+//#ExpectSym:aligned_data alignment=2048
 
 __attribute__((section(".data.aligned"))) int aligned_data = 42;
 void _start() {}
